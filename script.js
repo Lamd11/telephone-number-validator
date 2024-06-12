0f2fa07393be40ef96a3506e6cc093e83e3607ea
Daniel <daniel.lam3987@gmail.com>
2024-06-12 15:33:38 -0400

    Completed script.js.

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
 
-
+const check = (inputString) =>{
+    const regex = /^(\s*)?(1?)?(\s*)?(\(\d{3}\)|\d{3})[\s-]?(\s*)?(\d{3}-?)(\s*)?(\d{4}-?)(\s*)?$/g;
+    const inputString_regex = inputString.replace(regex, "");
+    if(inputString_regex === ""){
+        results.innerHTML += `<p>Valid US Number: ${inputString}</p>`;
+    }
+    else{
+        results.innerHTML += `<p>Invalid US Number: ${inputString}</p>`;
+    }
+}
 
 
 clearBtn.addEventListener("click", (e) =>{
@@ -13,5 +22,8 @@ clearBtn.addEventListener("click", (e) =>{
 checkBtn.addEventListener("click", (event) =>{
     if(!input.value){
         alert("Please provide a phone number");
+    }
+    else{
+        check(input.value);
     }
 });
